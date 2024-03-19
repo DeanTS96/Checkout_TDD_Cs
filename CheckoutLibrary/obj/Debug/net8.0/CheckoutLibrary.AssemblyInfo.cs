8a34900e8e683d3af29b4dd156ba45d87c2aae22
Dean <dean96@live.co.uk>
2024-03-19 13:46:05 +0000

    added test for scan method when passed empty string

--- CheckoutLibrary/obj/Debug/net8.0/CheckoutLibrary.AssemblyInfo.cs
+++ CheckoutLibrary/obj/Debug/net8.0/CheckoutLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckoutLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05523121bb7c0d5c59c30f3ce96aba6e6c486177")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckoutLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckoutLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CheckoutLibraryTests/CheckoutUtilities.Tests/Checkout/CheckoutTests.cs
+++ CheckoutLibraryTests/CheckoutUtilities.Tests/Checkout/CheckoutTests.cs
@@ -5,8 +5,12 @@ namespace CheckoutLibraryTests;
 public class CheckoutMethodTests
 {
     [TestMethod]
-    public void FirstTest()
+    public void ScanMethod_When_Passed_Nothing_Do_Nothing()
     {
-        
+        Checkout checkout = new Checkout();
+        checkout.Scan("");
+        bool IsInItemList = checkout.ItemList.ContainsKey("");
+
+        Assert.IsFalse(IsInItemList, $"Scanned item should not be added to ItemList: expected false; Actual: {IsInItemList}");
     }
 }
