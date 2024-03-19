0b9387516594d4a4ebcb360314797cdad72723f1
Dean <dean96@live.co.uk>
2024-03-19 16:50:09 +0000

    implimented RegexPatterns in Utils folder, to be used in Checkout, rather that extra code in Checkout

--- CheckoutLibrary/obj/Debug/net8.0/CheckoutLibrary.AssemblyInfo.cs
+++ CheckoutLibrary/obj/Debug/net8.0/CheckoutLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckoutLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c5dee965b5f52d636cee3e823f42f91f32e5371")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3098f01f829e618c2d5baeb68f798d445e531554")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckoutLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckoutLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
