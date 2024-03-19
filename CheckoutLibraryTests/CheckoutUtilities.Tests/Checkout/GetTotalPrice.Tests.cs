f37e1f5433ace7c3c07a941e0041a128d1f4e976
Dean <dean96@live.co.uk>
2024-03-19 20:22:17 +0000

    Added first test for get total price for if nothing is scanned

---
+++ CheckoutLibraryTests/CheckoutUtilities.Tests/Checkout/GetTotalPrice.Tests.cs
@@ -0,0 +1,23 @@
+using CheckoutLibrary;
+namespace CheckoutLibraryTests;
+
+[TestClass]
+public class GetTotalPriceTests
+{
+    Dictionary<string, int> prices = new Dictionary<string, int>()
+    {
+        {"A", 30}
+    };
+    Dictionary<string, int> specialPrices = new Dictionary<string, int>();
+    //Happy Tests
+    [TestMethod]
+    public void Method_Returns_0_When_No_Items_Have_Been_Scanned()
+    {
+        Checkout checkout = new Checkout(prices, specialPrices);
+        int expected = 0;
+        int actual = checkout.GetTotalPrice();
+
+        Assert.AreEqual(expected, actual, $"Scanned item is added to ItemList. EXPECTED: {expected}; ACTUAL: {actual}");
+    }
+    //Bad Tests
+}
