2d46a998a566854bc12cee13aecf6f8fc792c5e1
Dean <dean96@live.co.uk>
2024-03-19 16:19:05 +0000

    adding so I can pull from main

--- CheckoutLibraryTests/obj/Debug/net8.0/CheckoutLibraryTests.AssemblyInfo.cs
+++ CheckoutLibraryTests/obj/Debug/net8.0/CheckoutLibraryTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckoutLibraryTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05523121bb7c0d5c59c30f3ce96aba6e6c486177")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckoutLibraryTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckoutLibraryTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
