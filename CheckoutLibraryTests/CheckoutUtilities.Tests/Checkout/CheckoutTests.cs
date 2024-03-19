4292c1bbbcc29c45a0f2bdf18978067511e615c2
Dean <dean96@live.co.uk>
2024-03-19 14:56:16 +0000

    added test to check lowercase letters don't get added to ItemList

--- CheckoutLibraryTests/CheckoutUtilities.Tests/Checkout/CheckoutTests.cs
+++ CheckoutLibraryTests/CheckoutUtilities.Tests/Checkout/CheckoutTests.cs
@@ -4,16 +4,8 @@ namespace CheckoutLibraryTests;
 [TestClass]
 public class CheckoutMethodTests
 {
-    [TestMethod]
-    public void ScanMethod_When_Passed_Nothing_Do_Nothing()
-    {
-        Checkout checkout = new Checkout();
-        checkout.Scan("");
-        bool IsInItemList = checkout.ItemList.ContainsKey("");
-
-        Assert.IsFalse(IsInItemList, $"Scanned item of an empty string should not be added to ItemList. EXPECTED: false; ACTUAL: {IsInItemList}");
-    }
-    [TestMethod]
+    //Happy Tests
+        [TestMethod]
     public void ScanMethod_When_Passed_Single_Character_Adds_Item_To_ItemList()
     {
         Checkout checkout = new Checkout();
@@ -22,7 +14,17 @@ public class CheckoutMethodTests
 
         Assert.IsTrue(IsInItemList, $"Scanned item is added to ItemList. EXPECTED: true; ACTUAL: {IsInItemList}");
     }
-    [TestMethod]
+    //Bad Tests
+        [TestMethod]
+    public void ScanMethod_When_Passed_Nothing_Do_Nothing()
+    {
+        Checkout checkout = new Checkout();
+        checkout.Scan("");
+        bool IsInItemList = checkout.ItemList.ContainsKey("");
+
+        Assert.IsFalse(IsInItemList, $"Scanned item of an empty string should not be added to ItemList. EXPECTED: false; ACTUAL: {IsInItemList}");
+    }
+        [TestMethod]
     public void ScanMethod_When_Passed_Not_A_Letter_Does_Nothing()
     {
         Checkout checkout = new Checkout();
@@ -30,5 +32,13 @@ public class CheckoutMethodTests
         bool IsInItemList = checkout.ItemList.ContainsKey(";");
 
         Assert.IsFalse(IsInItemList, $"If scaned item contains non letters, it is not added to ItemList. EXPECTED: false; ACTUAL: {IsInItemList}");
+    }
+        public void ScanMethod_When_Passed_Lower_Case_Letters_Does_Nothing()
+    {
+        Checkout checkout = new Checkout();
+        checkout.Scan("a");
+        bool IsInItemList = checkout.ItemList.ContainsKey("a");
+
+        Assert.IsFalse(IsInItemList, $"If scaned item contains lower case letters, it is not added to ItemList. EXPECTED: false; ACTUAL: {IsInItemList}");
     }
 }
