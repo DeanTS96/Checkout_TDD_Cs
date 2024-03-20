473bb86e50e66765fa35070ae91673c76fdc5969
Dean <dean96@live.co.uk>
2024-03-20 20:52:50 +0000

    Fixed details in the program to run the checkout instance properly

--- Checkout/Program.cs
+++ Checkout/Program.cs
@@ -4,10 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Dictionary<string, int> prices = new Dictionary<string, int>();
-        Dictionary<string, int> specialPrices = new Dictionary<string, int>();
+        Dictionary<string, int> prices = new Dictionary<string, int>()
+        {
+            {"A", 30}
+        };
+        Dictionary<string, Dictionary<string, int>> specialPrices = new Dictionary<string, Dictionary<string, int>>();
         Checkout checkout = new Checkout(prices, specialPrices);
-        checkout.Scan("B");
-        Console.WriteLine(checkout.ItemList["B"]);
+        checkout.Scan("A");
+        Console.WriteLine(checkout.ItemList["A"]);
     }
 }
